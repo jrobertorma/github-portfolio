db97300407f30a0cb8550cb705ce1b26840f3455
jrobertorma <jrobertorma@gmail.com>
2021-08-06 10:01:38 -0500

    Adding links to profile component

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,24 +1,46 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
 import { Container } from '@material-ui/core';
 
+const ProfileLink = ({profileLinkData}) => {
+    return (
+        <Typography variant="body1" color="textSecondary" align="justify">
+            <strong>{profileLinkData.profileLinkTitle}: </strong> 
+            <Link href={profileLinkData.profileLink} target="_blank" rel="noreferrer">
+                {profileLinkData.profileLink}
+            </Link>
+        </Typography>
+    );
+}
+
 const Profile = ({profileData}) => {
+    console.log(profileData); 
+    //convertir en array, luego crear un array de objetos del tipo [{profileLinkTitle: 'val', profileLink:'val'}], 
+    //luego recorrer nuevo array con map y pasárlo a ProfileLink ;)
+
     return (
         <Container maxWidth="sm">
             <Typography variant="h4" component="h4" gutterBottom>
                 Profile
             </Typography>
             <Typography variant="body1" color="textSecondary" align="justify">
-                html_url: {profileData.html_url}
+                <strong>Pofile url: </strong> 
+                <Link href={profileData.html_url} target="_blank" rel="noreferrer">
+                    {profileData.html_url}
+                </Link>
             </Typography>
             <Typography variant="body1" color="textSecondary" align="justify">
-                repos_url: {profileData.repos_url}
+                <strong>Repositories url: </strong> 
+                <Link href={profileData.repos_url} target="_blank" rel="noreferrer">
+                    {profileData.repos_url}
+                </Link>
             </Typography>
             <Typography variant="body1" color="textSecondary" align="justify">
-                name: {profileData.name}
-            </Typography>
-            <Typography variant="body1" color="textSecondary" align="justify">
-                company: {profileData.company}
+                <strong>Company: </strong> 
+                <Link href={profileData.company} target="_blank" rel="noreferrer">
+                    {profileData.company}
+                </Link>
             </Typography>
             <Typography variant="body1" color="textSecondary" align="justify">
                 location: {profileData.location}
